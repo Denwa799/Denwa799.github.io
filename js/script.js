ee2e330e8ce2712f5c5da7d9ac1b3864db26542c
Denwa799 <69731012+Denwa799@users.noreply.github.com>
2020-09-13 18:33:56 +0300

    Preloader

--- js/script.js
+++ js/script.js
@@ -1,134 +1,145 @@
 // Анимация частиц для мобильных
 
 (function () {
-    let canvas = document.querySelector('.particlesMobile'),
-        ctx = canvas.getContext('2d'),
-        w = canvas.width = window.innerWidth,
-        h = canvas.height = window.innerHeight,
-        particles = [],
-        properties = {
-            bgColor: 'rgba(17, 17, 19, 1)',
-            particleColor: 'rgba(255, 40, 40, 1)',
-            particleRadius: 3,
-            particleCount: 60,
-            particleMaxVelocity: 0.5,
-            lineLength: 150,
-            particleLife: 6,
-        };
+  let canvas = document.querySelector('.particlesMobile'),
+  ctx = canvas.getContext('2d'),
+  w = canvas.width = window.innerWidth,
+  h = canvas.height = window.innerHeight,
+  particles = [],
+  properties = {
+    bgColor: 'rgba(17, 17, 19, 1)',
+    particleColor: 'rgba(255, 40, 40, 1)',
+    particleRadius: 3,
+    particleCount: 60,
+    particleMaxVelocity: 0.5,
+    lineLength: 150,
+    particleLife: 6,
+  };
 
 
-    window.onresize = function () {
-        w = canvas.width = innerWidth,
-        h = canvas.height = innerHeight;
+  window.onresize = function () {
+    w = canvas.width = innerWidth,
+    h = canvas.height = innerHeight;
+  }
+
+  class Particle {
+    constructor() {
+      this.x = Math.random() * w;
+      this.y = Math.random() * h;
+      this.velocityX = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
+      this.velocityY = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
+      this.life = Math.random() * properties.particleLife * 60;
     }
 
-    class Particle {
-        constructor() {
-            this.x = Math.random() * w;
-            this.y = Math.random() * h;
-            this.velocityX = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
-            this.velocityY = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
-            this.life = Math.random() * properties.particleLife * 60;
-        }
-
-        position() {
-            this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX < 0 ? this.velocityX *= -1 : this.velocityX;
-            this.y + this.velocityY > h && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY < 0 ? this.velocityY *= -1 : this.velocityY;
-            this.x += this.velocityX;
-            this.y += this.velocityY;
-        }
-
-        reDraw() {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, properties.particleRadius, 0, Math.PI * 2);
-            ctx.closePath();
-            ctx.fillStyle = properties.particleColor;
-            ctx.fill();
-        }
-
-        reCalculateLife() {
-            if (this.life < 1) {
-                this.x = Math.random() * w;
-                this.y = Math.random() * h;
-                this.velocityX = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
-                this.velocityY = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
-                this.life = Math.random() * properties.particleLife * 60;
-            }
-            this.life--;
-        }
+    position() {
+      this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX < 0 ? this.velocityX *= -1 : this.velocityX;
+      this.y + this.velocityY > h && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY < 0 ? this.velocityY *= -1 : this.velocityY;
+      this.x += this.velocityX;
+      this.y += this.velocityY;
     }
 
-    function reDrawBackground() {
-        ctx.fillStyle = properties.bgColor;
-        ctx.fillRect(0, 0, w, h);
+    reDraw() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, properties.particleRadius, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.fillStyle = properties.particleColor;
+      ctx.fill();
     }
 
-    function drawLines() {
-        let x1, y1, x2, y2, length, opacity;
-        for (let i in particles) {
-            for (let j in particles) {
-                x1 = particles[i].x;
-                y1 = particles[i].y;
-                x2 = particles[j].x;
-                y2 = particles[j].y;
-                length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-                if (length < properties.lineLength) {
-                    opacity = 1 - length / properties.lineLength;
-                    ctx.lineWidth = '0.5';
-                    ctx.strokeStyle = 'rgba(255, 40, 40, ' + opacity + ')';
-                    ctx.beginPath();
-                    ctx.moveTo(x1, y1);
-                    ctx.lineTo(x2, y2);
-                    ctx.closePath();
-                    ctx.stroke();
-                }
-            }
+    reCalculateLife() {
+      if (this.life < 1) {
+        this.x = Math.random() * w;
+        this.y = Math.random() * h;
+        this.velocityX = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
+        this.velocityY = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity;
+        this.life = Math.random() * properties.particleLife * 60;
+      }
+      this.life--;
+    }
+  }
+
+  function reDrawBackground() {
+    ctx.fillStyle = properties.bgColor;
+    ctx.fillRect(0, 0, w, h);
+  }
+
+  function drawLines() {
+    let x1, y1, x2, y2, length, opacity;
+    for (let i in particles) {
+      for (let j in particles) {
+        x1 = particles[i].x;
+        y1 = particles[i].y;
+        x2 = particles[j].x;
+        y2 = particles[j].y;
+        length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        if (length < properties.lineLength) {
+          opacity = 1 - length / properties.lineLength;
+          ctx.lineWidth = '0.5';
+          ctx.strokeStyle = 'rgba(255, 40, 40, ' + opacity + ')';
+          ctx.beginPath();
+          ctx.moveTo(x1, y1);
+          ctx.lineTo(x2, y2);
+          ctx.closePath();
+          ctx.stroke();
         }
+      }
     }
+  }
 
-    function reDrawParticles() {
-        for (var i in particles) {
-            particles[i].reCalculateLife();
-            particles[i].position();
-            particles[i].reDraw();
-        }
+  function reDrawParticles() {
+    for (var i in particles) {
+      particles[i].reCalculateLife();
+      particles[i].position();
+      particles[i].reDraw();
     }
+  }
 
-    function loop() {
-        reDrawBackground();
-        reDrawParticles();
-        drawLines();
-        requestAnimationFrame(loop);
+  function loop() {
+    reDrawBackground();
+    reDrawParticles();
+    drawLines();
+    requestAnimationFrame(loop);
+  }
+
+  function init() {
+    for (let i = 0; i < properties.particleCount; i++) {
+      particles.push(new Particle);
     }
+    loop();
+  }
 
-    function init() {
-        for (let i = 0; i < properties.particleCount; i++) {
-            particles.push(new Particle);
-        }
-        loop();
-    }
-
-    init();
+  init();
 
 }());
+
+//preloader
+
+document.body.onload = function() {
+  setTimeout(function() {
+    let preloader = document.getElementById('page__preloader');
+    if (!preloader.classList.contains('done')) {
+      preloader.classList.add('done');
+    }
+  }, 1000);
+}
 
 //slider
 
 $(document).ready(function () {
-    $('.slider').slick({
-        arrows: true,
-        slidesToShow: 4,
-        infinite: false,
-        touchThreshold: 20,
-        responsive: [
-            {
-                breakpoint: 1000,
-                settings: {
-                    slidesToShow: 3
-                }
-            }
-        ]
-    });
+  $('.slider').slick({
+    arrows: true,
+    slidesToShow: 4,
+    infinite: false,
+    touchThreshold: 20,
+    responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 3
+      }
+    }
+    ]
+  });
 });
 
 //Плавная прокрутка к якорю
@@ -136,135 +147,135 @@ $(document).ready(function () {
 const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
-    anchor.addEventListener("click", function (event) {
-        event.preventDefault();
-        const blockID = anchor.getAttribute('href')
-        document.querySelector('' + blockID).scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        })
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href')
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
     })
+  })
 }
 
 
 //Анимация частиц
 
 const particlesSettings = {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ff2828"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 1,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
         "enable": true,
-        "distance": 150,
-        "color": "#ff2828",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 4,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+        "value_area": 800
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
+    "color": {
+      "value": "#ff2828"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
+    "opacity": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ff2828",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 4,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
     }
-  };
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+  "config_demo": {
+    "hide_card": false,
+    "background_color": "",
+    "background_image": "",
+    "background_position": "50% 50%",
+    "background_repeat": "no-repeat",
+    "background_size": "cover"
+  }
+};
 
 particlesJS('particles', particlesSettings)
 
@@ -275,6 +286,6 @@ const menuToggle = document.querySelector('#menu-togle');
 const mobileNavContainer = document.querySelector('#mobile__nav');
 
 menuToggle.onclick = function () {
-    menuToggle.classList.toggle('menu-icon-active');
-    mobileNavContainer.classList.toggle('mobile__nav-active');
+  menuToggle.classList.toggle('menu-icon-active');
+  mobileNavContainer.classList.toggle('mobile__nav-active');
 }
